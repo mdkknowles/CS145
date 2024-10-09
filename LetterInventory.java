766cc810757f6105e3ed026358e748222179ee67
JasiaErnest <119990861+JasiaErnest@users.noreply.github.com>
2024-10-09 08:24:46 -0700

    Create LetterInventory.java

---
+++ LetterInventory.java
@@ -0,0 +1,51 @@
+// Name: Jasia Ernest, Abdul Rahman, and Micheal Knowles
+// Assignment: Lab 3 - Letter Inventory
+// Date: October 11 2024
+// Class: CS&145
+
+public class LetterInventory {
+    /* This method constructs an inventory (a count) of the alphabetic letters in the given string, 
+    ignoring the case of letters and ignoring any non-alphabetic characters. */
+    public LetterInventory(String data) {
+
+    }
+
+    // This method returns a count of how many of this letter are in the inventory
+    public int get(char letter) {
+
+    }
+
+    // This method sets the count for the given letter to the given value.
+    public void set(char letter, int value) {
+
+    }
+
+    // This method returns the sum of all of the counts in this inventory
+    public int size() {
+
+    }
+    
+    // This method returns true if this inventory is empty which means that all counts are 0
+    public boolean isEmpty() {
+
+    }
+
+    /* This method returns a String representation of the inventory with the letters all in lowercase 
+    and in sorted order and surrounded by square brackets */
+    public String toString() {
+
+    }
+
+    /* This method constructs and returns a new LetterInventory object 
+    that represents the sum of this letter inventory and the other given LetterInventory */
+    public LetterInventory add() {
+
+    }
+
+    /* This method constructs and returns a new LetterInventory object that represents 
+    the result of subtracting the other inventory from this inventory 
+    (i.e., subtracting the counts in the other inventory from this object’s counts) */
+    public LetterInventory substract() {
+
+    }
+}
